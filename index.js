7941cef2726466ad3c647e9f1535c58181674607
Vishal D <vishaldiwadkar@protonmail.com>
2018-08-25 20:33:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,5 @@ function sayHiToGrandma(string) {
   {
       return "I can't hear you!"
   }
-}
-
-function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
-  {
-      return "YES INDEED!"
-  }
 }
