c47ca84fd11a8c8aed50affe6889af72c0d67ec3
Vishal D <vishaldiwadkar@protonmail.com>
2018-08-25 20:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(string) {
   {
     return "YES INDEED!"
   }
-    else if (string === string.toUpperCase())
+    else if (string === "I love you, Grandma.")
   {
-    return "YES INDEED!"
+    return "I love you, too."
   }
 }
